c8bab6d143a2128e50e1ce65424c77c9d0d587ea
hbabuka <hristijan.babuka@gmail.com>
2022-03-12 01:17:13 +0100

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./styles/GlobalStyles";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <>
         <GlobalStyle />
+        <Nav />
         <AboutUs />
       </>
     </div>
