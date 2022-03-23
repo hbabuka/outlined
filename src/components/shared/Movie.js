d192c8edc20df724d73fd468dc4b1cd6c4406141
hbabuka <hristijan.babuka@gmail.com>
2022-03-23 01:55:21 +0100

    Our Work animations

--- src/components/shared/Movie.js
+++ src/components/shared/Movie.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import {
+  pageAnimation,
+  fade,
+  photoAnimation,
+  lineAnimation,
+} from "../../animation";
+import { motion } from "framer-motion";
 
 const DivStyled = styled.div`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
-    background-color: #cccccc;
+    background-color: #23d997;
     margin-bottom: 3rem;
   }
   img {
@@ -16,13 +23,23 @@ const DivStyled = styled.div`
   }
 `;
 
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
 export const Movie = ({ title, imageSource, linkTo }) => {
   return (
     <DivStyled>
-      <h2>{title}</h2>
-      <div className="line"></div>
+      <motion.h2 variants={fade}>{title}</motion.h2>
+      <motion.div className="line" variants={lineAnimation}></motion.div>
       <Link to={linkTo}>
-        <img src={imageSource} alt={imageSource} />
+        <Hide>
+          <motion.img
+            src={imageSource}
+            alt={imageSource}
+            variants={photoAnimation}
+          />
+        </Hide>
       </Link>
     </DivStyled>
   );
